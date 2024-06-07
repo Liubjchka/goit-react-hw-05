1f5ce994c64fc279f6d924a40eb9591c2cd968cc
Liubjchka <144176266+Liubjchka@users.noreply.github.com>
2024-06-07 18:36:11 +0300

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 
-import { Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 
-// http://localhost:5173/
+// http://localhost:5173/ - початкова сторінка/home
+// http://localhost:5173/about 
+// http://localhost:5173/profile
+
 
 export const App = () => {
 
   return (
     <div>
     <h1>Ruting is React</h1>
-    <Routes></Routes>
+    <Routes>
+      <Route path='/' element={<div>Home page</div>} />
+      <Route path='/about' element={<div>About</div>}/>
+      <Route path='/profile' element={<div>Profile</div>}/>
+    </Routes>
     </div>
   )
   
