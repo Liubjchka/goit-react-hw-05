ce1815f7830a224366d15485bd598d75ea893721
Liubjchka <144176266+Liubjchka@users.noreply.github.com>
2024-06-07 15:44:29 +0300

    install

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import { Routes } from 'react-router-dom'
+import './App.css'
+
+// http://localhost:5173/
+
+export const App = () => {
+
+  return (
+    <div>
+    <h1>Ruting is React</h1>
+    <Routes></Routes>
+    </div>
+  )
+  
+}
+
