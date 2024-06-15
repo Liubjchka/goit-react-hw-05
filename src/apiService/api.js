ec634326b4886b093caece1154ec0f958a8ce3ba
Liubjchka <144176266+Liubjchka@users.noreply.github.com>
2024-06-15 03:06:53 +0300

    add posters to movie

--- src/apiService/api.js
+++ src/apiService/api.js
@@ -1,9 +1,18 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://api.themoviedb.org'
+const BASE_URL = 'https://api.themoviedb.org'
+const API_KEY = '33093cbbc00611921b04f9776f3628fe'
+
+
+const options = {
+headers: {
+Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMzA5M2NiYmMwMDYxMTkyMWIwNGY5Nzc2ZjM2MjhmZSIsInN1YiI6IjY2NjRjOGUyY2UzNjYzNDllMjZkZTRlYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.78mug-BZWcIQKq-QyfQ3vuH5OLbtlehnEaVauoU2Xmo'
+}
+};
 
 export const getMovies = async ({abortController}) => {
-    const response = await axios.get('3/trending/movie/day?api_key=33093cbbc00611921b04f9776f3628fe', 
+    const url = `${BASE_URL}/3/trending/movie/day?language=en-US?api_key=${API_KEY}`
+    const response = await axios.get(url, options, 
         {
             signal: abortController.signal
         }
@@ -11,5 +20,9 @@ export const getMovies = async ({abortController}) => {
     return response.data.results;
 }
 
-
-// Питання по baseURL
+export const getMovieById = async movieId => {
+    const url = `${BASE_URL}/3/movie/${movieId}?language=en-US?api_key=${API_KEY}`
+    const response = await axios.get(url, options)
+    return response.data;
+}
+// Питання по baseURL, api_key
--- src/pages/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage.jsx
@@ -1,11 +1,51 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
+import { getMovieById } from "../apiService/api";
 
 export default function MovieDetailsPage() {
-const params = useParams();
+const {moviesId} = useParams();
 
-console.log(params);
+const [movie, setMovie] = useState(null);
+const [error, setError] = useState(null);
+
+useEffect(()=>{
+    async function fetchData() {
+        try {
+            const fetchedMovie = await getMovieById(moviesId);
+            setMovie(fetchedMovie);
+        } catch (error) {
+            setError(error.message)
+        }
+        }
+            fetchData();
+        }, [moviesId]);
 
     return (
-        <div>MovieDetailsPage - {params.moviesId}</div>
+        <div>
+        <h1>MovieDetailsPage</h1>
+        {error && <p>Error fetching movie: {error}</p>}
+
+        {movie && (<div>
+            <p>ID: {movie.id}</p>
+            <p>Title: {movie.title || movie.name}</p>
+
+<p>
+            Poster:
+            {movie.poster_path && (
+            <img
+                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                alt={movie.title || movie.name}
+            />
+            )}
+            </p>
+
+        </div>)}
+        </div>
     )
 }
+
+
+// "id": 1022790,
+// "name": "Inside Out Collection",
+// "poster_path": "/Apr19lGxP7gm6y2HQX0kqOXTtqC.jpg",
+// "backdrop_path":  "/bR8H6S3Jc2vhPvz2VWX0dQ3XTFV.jpg"
