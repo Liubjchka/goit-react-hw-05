6796c968b310362b834e96d1b97272179e59b1c9
Liubjchka <144176266+Liubjchka@users.noreply.github.com>
2024-06-18 02:07:29 +0300

    add good filter

--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -1,20 +1,60 @@
 import { useSearchParams } from "react-router-dom";
 import { Filter } from "../components/Filter/Filter";
+import { useEffect, useState } from "react";
+import { MovieList } from "../components/MovieList/MovieList";
+import { getMovies } from "../apiService/api";
 
 export default function MoviesPage() {
+const [movies, setMovies] = useState([]);
+const [error, setError] = useState(false)
 
 const [params, setParams] = useSearchParams();
 //об'єкт, що описує URL;
-
 const filter = params.get("filter") ?? "";
+
+useEffect(()=>{
+if (filter === "") {
+    setMovies([]);
+    return;
+}
+
+
+    const controller = new AbortController();
+    
+        async function fetchData() {
+            try {
+                const fetchMovies = await getMovies({
+                    abortController : controller,
+                });
+                setMovies(fetchMovies)
+            } catch (error) {
+                if (error.code !== 'ERR_CANCELED') {
+                    // console.log(error);
+                    setError(true);
+                }
+            }
+        }
+    fetchData();
+    
+    return () =>{
+        controller.abort()
+    }
+    }, [filter])
+
 
 const changeFilter = (newFilter) => {
 params.set('filter', newFilter)
 setParams(params)
 }
+
+const  filteredMovies = movies.filter(movie => movie.title.toLowerCase().includes(filter.toLowerCase()))
+
+
     return (
         <div>
-            <Filter value={filter} onChange={changeFilter}/>
-        </div>
+        <Filter value={filter} onChange={changeFilter} />
+        {error && <p>OOPS! ERROR!</p>}
+        {filteredMovies.length > 0 && <MovieList movies={filteredMovies} />}
+      </div>
     )
 }
